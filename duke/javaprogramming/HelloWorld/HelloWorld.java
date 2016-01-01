b13d50cc728e9c5b212c8afb7c1bde0b85684b82
YaffyKoyo <qyaffy@gmail.com>
2016-01-01 23:44:27 +0000

    some examples

---
+++ duke/javaprogramming/HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of HelloWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+
+public class HelloWorld {
+	public void runHello () {
+		URLResource res = new URLResource("http://www.dukelearntoprogram.com/java/somefile.txt");
+		for (String line : res.words()) {
+			
+		    System.out.println(line);
+		}
+	}
+}
+
